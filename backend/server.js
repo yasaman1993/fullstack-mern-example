0a2308778086cbbf57b952f34b8be1d97d2fc740
dci-student <yasaman.nikpay@gmail.com>
2024-11-25 14:30:13 +0100

    update12

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ['http://localhost:5173', 'https://fullstack-mern-example-frontend-x052.onrender.com'],
     credentials: true,
   })
 );
